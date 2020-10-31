c4c0ab550b4a92f8c773064a13f849490f78ac43
manon.fouquet <manon.fouquet@artelys.com>
2020-10-31 21:41:13 +0100

    production environement updated with miniCSS and wb plugin

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -2,15 +2,41 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
+
+// npm i -D mini-css-extract-plugin optimize-css-assets-webpack-plugin terser-webpack-plugin workbox-webpack-plugin
+
+// const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+
+// Plugins
+const WorkboxPlugin = require('workbox-webpack-plugin');                                                       
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    // optimization: {
+        // minimize: true,
+        // minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    // },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                exclude: /node_modules/,
+                loader: "file-loader",
+                options: {
+                  name: '[path][name].[ext]',
+                }
             }
         ]
     },
@@ -18,6 +44,10 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
+
+
