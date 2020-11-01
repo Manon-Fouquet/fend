742671f5c7eacd2f46ddff9fd51aa8ece12d288a
manon.fouquet <manon.fouquet@artelys.com>
2020-11-01 15:05:46 +0100

    update tests

--- projects/evaluate-news-nlp/__test__/checkDefault.spec.js
+++ projects/evaluate-news-nlp/__test__/checkDefault.spec.js
@@ -1,4 +1,4 @@
-const checkNonEmpty = require('../js/nameChecker.js');
+const checkNonEmpty = require('../src/client/js/nameChecker.js');
 
 test('Retrieve default test returns ',()=>{
     expect(checkNonEmpty('test').not.toBe('test'))
--- projects/evaluate-news-nlp/__test__/formHandler.check.js
+++ projects/evaluate-news-nlp/__test__/formHandler.check.js
@@ -1,4 +1,4 @@
-const retrieveData = require('../js/formHandler.js');
+const retrieveData = require('../src/client/js/formHandler.js');
 
 test('Retrieve default test returns ',()=>{
     expect(retrieveData('test').toBe('The text is subjective, nonironic and expresses agreement.'))
